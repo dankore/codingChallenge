a99c4a7b9999fd680aea18c1056965ea21c81a55
Adamu M. Dankore <adamu.dankore@gmail.com>
2020-01-11 23:09:16 -0600

    cards now smaller

--- frontend-js/modules/typeJ.js
+++ frontend-js/modules/typeJ.js
@@ -31,20 +31,14 @@ export default class TypeJ {
       .map(object => {
         if (object.type == "typeJ") {
           return `
-             <div class="max-w-sm rounded overflow-hidden bg-green-500 shadow-lg m-3 px-6 py-4 text-center">
-              <p class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-3">
-                  ${object.id}
-              </p>
-              <h2 class="font-bold text-xl mb-2"> ${object.type}</h2>
+            <div class="text-l font-bold bg-blue-500 rounded-full shadow-lg m-2 p-2 text-white">
+              <p>${object.type}</p>
             </div>
           `;
         } else {
           return `
-            <div class="max-w-sm rounded overflow-hidden shadow-lg m-3 bg-white px-6 py-4 text-center">
-              <p class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-3">
-                  ${object.id}
-              </p>
-              <div class="font-bold text-xl mb-2"> ${object.type}</div>
+            <div class="text-l font-semibold text-blue-400 rounded-full shadow-lg m-2 p-2 bg-white">
+              <p>${object.type}</p>
             </div>
               `;
         }
